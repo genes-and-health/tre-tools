4fe59db59ee68f998c51a3eb652f4007c0a0bd0c
Caroline Morton <carolinemorton@gmx.com>
2023-10-23 17:01:54 +0100

    add test data

---
+++ tests/datasets/test_base.py
@@ -0,0 +1,128 @@
+import pytest
+import os
+import csv
+
+from tretools.datasets.base import Dataset
+from tretools.datasets.errors import DatasetPathNotCorrect, DatasetPathNotCorrect, WriteOptionsInvalid, UnsupportedFileType
+
+
+def test_good_dataset():
+    observed_dataset = Dataset(path="tests/test_data/primary_care/procedures.csv", dataset_type="primary_care", coding_system="SNOMED")
+    assert observed_dataset.dataset_type == "primary_care"
+    assert observed_dataset.coding_system == "SNOMED"
+
+    # assert column names are correct
+    assert observed_dataset.data.columns == ["pseudo_nhs_number", "id", "clinical_effective_date", "original_code", "original_term"]
+    
+    # Check first row of data is correct
+    first_row = observed_dataset.data.slice(0, 1)
+    assert first_row["pseudo_nhs_number"][0] == "84950DE0614A5C241F7223FBCCD27BE87DB61915972C7E49EDF519B72A3A104A"
+    assert first_row["clinical_effective_date"][0] == "2018-10-05"
+    assert first_row["original_code"][0] == 100000001
+    assert first_row["original_term"][0] == "Disease A - 1"
+
+
+def test_bad_path_dataset():
+    with pytest.raises(DatasetPathNotCorrect) as e:
+        observed_dataset = Dataset(path="bad/path/to/dataset.csv", dataset_type="primary_care", coding_system="SNOMED")
+
+    assert "Invalid path for Dataset: bad/path/to/dataset.csv" in str(e.value)
+
+
+def test_processed_log():
+    ingested_data = Dataset(path="tests/test_data/primary_care/procedures_many_diffs.csv", dataset_type="primary_care", coding_system="SNOMED")
+    ingested_data.log.append("test log")
+
+    assert ingested_data.log == ["test log"]
+    assert len(ingested_data.log) == 1
+
+
+def test_log_gets_written():
+    ingested_data = Dataset(path="tests/test_data/primary_care/procedures_many_diffs.csv", dataset_type="primary_care", coding_system="SNOMED")
+    ingested_data.log.append("first test log")
+    ingested_data.log.append("second test log")
+    
+    # write the log
+    ingested_data.write_to_log("tests/test_data/primary_care/test_log.txt", overwrite_or_append="overwrite")
+
+    # read in the log and check it is correct
+    with open("tests/test_data/primary_care/test_log.txt", "r") as f:
+        log = f.read()
+
+    # check first line and how many lines in the log - should be 7 (6 logs and empty line)
+    assert "first test log" in log
+    assert len(log.split("\n")) == 3
+
+    # write the log again but this time append
+    ingested_data.write_to_log("tests/test_data/primary_care/test_log.txt", overwrite_or_append="append")
+
+    # check how many lines in the log - should be 5 (2 logs * 2 and one empty line)
+    with open("tests/test_data/primary_care/test_log.txt", "r") as f:
+        log = f.read()
+    
+    assert len(log.split("\n")) == 5
+
+    # delete the log file
+    os.remove("tests/test_data/primary_care/test_log.txt")
+
+
+def test_log_with_wrong_args():
+    with pytest.raises(WriteOptionsInvalid) as e:
+        ingested_data = Dataset(path="tests/test_data/primary_care/procedures_many_diffs.csv", dataset_type="primary_care", coding_system="SNOMED")
+        ingested_data.log.append("first test log")
+        ingested_data.write_to_log("tests/test_data/primary_care/log.txt", overwrite_or_append="wrong")
+
+    assert "Invalid option for overwrite_or_append. Must be either 'overwrite' or 'append'" in str(e.value)
+
+
+def test_raises_error_if_not_csv_or_feather():
+    with pytest.raises(UnsupportedFileType) as e:
+        ingested_data = Dataset(path="tests/test_data/primary_care/fake_data.txt", dataset_type="primary_care", coding_system="SNOMED")
+
+    assert "File type not supported. Must be either .csv or .arrow" in str(e.value)
+
+
+def test_writes_csv():
+    ingested_data = Dataset(path="tests/test_data/primary_care/processed_data.csv", dataset_type="primary_care", coding_system="SNOMED")
+
+    # write the processed data to csv
+    ingested_data.write_to_csv("tests/test_data/primary_care/test_data.csv")
+
+    # read in the csv and check it is correct using csv reader. I am using csv reader
+    # to avoid a dependency on ProcessedDataset which is what we are testing. 
+    with open("tests/test_data/primary_care/test_data.csv", "r") as f:
+        reader = csv.reader(f)
+        rows = list(reader)
+
+    assert len(rows) == 8
+    assert rows[0] == ["nhs_number", "code", "term", "date"]
+    
+    os.remove("tests/test_data/primary_care/test_data.csv")
+
+def test_write_to_feather():
+    ingested_data = Dataset(path="tests/test_data/primary_care/processed_data.arrow", dataset_type="primary_care", coding_system="SNOMED")
+
+    # write to feather
+    ingested_data.write_to_feather("tests/test_data/primary_care/test_data.arrow")
+
+    # assert a file has been created
+    assert os.path.isfile("tests/test_data/primary_care/test_data.arrow")
+
+    # assert file not empty
+    assert os.stat("tests/test_data/primary_care/test_data.arrow").st_size != 0
+
+    # delete the file
+    os.remove("tests/test_data/primary_care/test_data.arrow")
+
+
+def test_read_from_csv():
+    # read the csv back in
+    loaded_data = Dataset(path="tests/test_data/primary_care/processed_data.csv", dataset_type="primary_care", coding_system="SNOMED")
+    assert loaded_data.data.shape == (7, 4)
+
+def test_read_from_feather():
+    # read the feather file back in
+    loaded_data = Dataset(path="tests/test_data/primary_care/processed_data.arrow", dataset_type="primary_care", coding_system="SNOMED")
+    assert loaded_data.data.shape == (7, 4)
+
+
---
+++ tests/datasets/test_processed_dataset.py
@@ -0,0 +1,91 @@
+import pytest
+import datetime
+
+from tretools.datasets.processed_dataset import ProcessedDataset
+from tretools.datasets.errors import DeduplicationError
+
+def test_load_processed_dataset():
+    observed_dataset = ProcessedDataset(path="tests/test_data/primary_care/processed_data.csv", dataset_type="primary_care", coding_system="SNOMED")
+    valid_column_names = observed_dataset._validate_column_names()
+    assert valid_column_names == True
+
+    # assert data shape
+    assert observed_dataset.data.shape == (7, 4)
+
+
+def test_merge_with_dataset():
+    observed_dataset_one = ProcessedDataset(path="tests/test_data/primary_care/processed_data.csv", dataset_type="primary_care", coding_system="SNOMED")
+    observed_dataset_two = ProcessedDataset(path="tests/test_data/primary_care/processed_data.csv", dataset_type="primary_care", coding_system="SNOMED")
+
+    observed_dataset_one.merge_with_dataset(observed_dataset_two)
+
+    # assert data shape
+    assert observed_dataset_one.data.shape == (14, 4)
+
+
+def test_merge_with_dataset_different_coding_system():
+    observed_dataset_one = ProcessedDataset(path="tests/test_data/primary_care/processed_data.csv", dataset_type="primary_care", coding_system="SNOMED")
+    observed_dataset_two = ProcessedDataset(path="tests/test_data/primary_care/processed_data.csv", dataset_type="primary_care", coding_system="ICD10")
+
+    with pytest.raises(DeduplicationError) as e:
+        observed_dataset_one.merge_with_dataset(observed_dataset_two)
+
+    assert "Coding system must be the same for both datasets" in str(e.value)
+
+
+def test_merge_with_dataset_different_dataset_type():
+    observed_dataset_one = ProcessedDataset(path="tests/test_data/primary_care/processed_data.csv", dataset_type="primary_care", coding_system="SNOMED")
+    observed_dataset_two = ProcessedDataset(path="tests/test_data/primary_care/processed_data.csv", dataset_type="secondary_care", coding_system="SNOMED")
+
+    with pytest.raises(DeduplicationError) as e:
+        observed_dataset_one.merge_with_dataset(observed_dataset_two)
+
+    assert "Dataset type must be the same for both datasets" in str(e.value)
+
+
+def test_merge_with_dataset_different_column_names():
+    observed_dataset_one = ProcessedDataset(path="tests/test_data/primary_care/processed_data.csv", dataset_type="primary_care", coding_system="SNOMED")
+    observed_dataset_two = ProcessedDataset(path="tests/test_data/primary_care/processed_data.csv", dataset_type="primary_care", coding_system="SNOMED")
+    # change the column name of the second dataset to check that it fails
+    observed_dataset_two.data = observed_dataset_two.data.rename({"nhs_number": "nhs_number_two"})
+
+    with pytest.raises(DeduplicationError) as e:
+        observed_dataset_one.merge_with_dataset(observed_dataset_two)
+
+    assert "Column names must be the same for both datasets" in str(e.value)
+
+
+def test_dedupe_no_date_limit():
+    observed_dataset_one = ProcessedDataset(path="tests/test_data/primary_care/processed_data.csv", dataset_type="primary_care", coding_system="SNOMED")
+    observed_dataset_two = ProcessedDataset(path="tests/test_data/primary_care/processed_data.csv", dataset_type="primary_care", coding_system="SNOMED")
+    observed_dataset_one.merge_with_dataset(observed_dataset_two)
+
+    dedup = observed_dataset_one.deduplicate()
+    assert dedup.data.shape == (5, 4)
+
+    # find row that has nhs_number = 84950DE0614A5C241F7223FBCCD27BE87DB61915972C7E49EDF519B72A3A104A
+    # and code = 100000001. Make sure that the date is the earliest date which is 2018-10-05
+    data = dedup.data
+    specific_row = data.filter((data["nhs_number"].eq("84950DE0614A5C241F7223FBCCD27BE87DB61915972C7E49EDF519B72A3A104A")) & (data["code"].eq("100000001")))
+    assert specific_row["date"][0] == "2018-10-05"
+
+    specific_row = data.filter((data["nhs_number"].eq("53952EF0503F7F341D9121DBCCC39DE95EA83713167E5E57EDB613A60D4C104C")) & (data["code"].eq("200000001")))
+    assert specific_row["date"][0] == "2016-07-19"
+
+    
+def test_dedupe_with_date_limit():
+    observed_dataset_one = ProcessedDataset(path="tests/test_data/primary_care/processed_data.csv", dataset_type="primary_care", coding_system="SNOMED")
+    observed_dataset_two = ProcessedDataset(path="tests/test_data/primary_care/processed_data.csv", dataset_type="primary_care", coding_system="SNOMED")
+    observed_dataset_one.merge_with_dataset(observed_dataset_two)
+
+    # deduplicate with date_start = 2018-10-06. This should drop most of the data
+    # except for 3 rows. 
+    dedup = observed_dataset_one.deduplicate(date_start="2018-10-06")
+    assert dedup.data.shape == (3, 4)
+
+    # find row that has nhs_number = 84950DE0614A5C241F7223FBCCD27BE87DB61915972C7E49EDF519B72A3A104A
+    # and code = 100000001. Make sure that the date is the earliest date which is 2018-11-05 
+    # which is the first date after the date_start
+    data = dedup.data
+    specific_row = data.filter((data["nhs_number"].eq("84950DE0614A5C241F7223FBCCD27BE87DB61915972C7E49EDF519B72A3A104A")) & (data["code"].eq("100000001")))
+    assert specific_row["date"][0] == "2018-11-05"
